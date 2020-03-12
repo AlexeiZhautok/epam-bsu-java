7f69e359acaf6330d1e3a13daea3a3c4c9e45115
Alexei Zheltok <alexeizhautok@gmail.com>
2020-03-12 21:57:43 +0300

    это описание

---
+++ LanguageLearningProject/src/com/company/model/Description.java
@@ -0,0 +1,12 @@
+package com.company.model;
+
+import javax.xml.crypto.Data;
+
+public class Description {
+    Data birthday;
+    String name;
+    String surname;
+    int age;
+    String discription;
+    String[] interesting;
+}
